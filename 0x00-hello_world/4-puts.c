ad5d6f93535e8781431a9cd10313b1783d77ee2c
sbonele <sbonelemiya@gmail.com>
2022-10-13 03:20:04 -0700

    task 4-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -desplaying
+ *
+ * Return:Always 0(passed)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -print sentence
+ *
+ * Return: Always 0 (passed)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
