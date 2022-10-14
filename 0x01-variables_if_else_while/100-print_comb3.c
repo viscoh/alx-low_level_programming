a8d4b5bcfb8694d4525568f79e158d8d5ac69b7b
sbonele <sbonelemiya@gmail.com>
2022-10-14 07:15:33 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits
+ * Return: Always
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			if (j > i)
+			{
+				putchar(i + '0');
+
+				putchar(j + '0');
+
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
