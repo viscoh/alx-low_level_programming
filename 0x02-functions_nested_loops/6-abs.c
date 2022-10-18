acc6221cca24cb20949d8d33469550ad55e03246
sbonele <sbonelemiya@gmail.com>
2022-10-18 06:26:14 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes
+ * @n: question
+ * Return: the abs of the int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-n);
+	}
+}
