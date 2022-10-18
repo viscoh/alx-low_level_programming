5208aeb36b2e43e6cd17ea204c407830135dddd8
sbonele <sbonelemiya@gmail.com>
2022-10-18 13:29:10 -0700

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,11 @@ int print_last_digit(int n)
 {
 	int x;
 
-	if (n < 0)
-	{
-		n = -n;
-	}
 	x = n % 10;
-	_putchar('0' + x);
-
-	return (x);
+	if (x < 0)
+	{
+		x *= -1;
+	}
+	_putchar(x + '0');
+	return (0);
 }
