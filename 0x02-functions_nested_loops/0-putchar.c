c0162b4a38709982d81cb4e7113b71e8d1a0a344
sbonele <sbonelemiya@gmail.com>
2022-10-18 04:04:08 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print
+ * Return: Always
+ */
+int  main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_purchar('t');
+	_purchar('c');
+	_purchar('h');
+	_purchar('a');
+	_purchar('r');
+	_purchar('\n');
+
+	return (0);
+}
