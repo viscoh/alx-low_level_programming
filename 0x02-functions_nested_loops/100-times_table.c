433246b3ed612edc8585d5dc5a006a851ff65982
sbonele <sbonelemiya@gmail.com>
2022-10-19 01:12:52 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,9 +31,7 @@ void print_times_table(int n)
 			}
 			else if (prod >= 100)
 			{
-				_putchar((prod / 100) + '0');
-				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+				_putchar(prod + '0');
 			}
 		}
 		_putchar('\n');
