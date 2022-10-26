fa918d2f98c6fc2c7ccc7c99e77576cad94b1b30
sbonele <sbonelemiya@gmail.com>
2022-10-26 12:45:01 -0700

    task 10

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,20 +10,22 @@ char *rot13(char *str)
 {
 	int index1, index2;
 
+	index1 = 0;
 	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 	char rot13key[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
 
-	while (str[++index1])
+	while (*(str + index1) != '\0')
 	{
 		for (index2 = 0; index2 < 52; index2++)
 		{
 			if (str[index1] == alphabet[index2])
 			{
-				str[index1] = rot13key[index2];
+				*(str + index1) = rot13key[index2];
 				break;
 			}
 		}
+		index1++;
 	}
 	return (str);
 }
