182e2ee74211829f8c2ba1d91ac50356b0f7abfd
sbonele <sbonelemiya@gmail.com>
2022-10-26 12:49:34 -0700

    task10

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -19,7 +19,7 @@ char *rot13(char *str)
 	{
 		for (index2 = 0; index2 < 52; index2++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (*(str + index1)  == alphabet[index2])
 			{
 				*(str + index1) = rot13key[index2];
 				break;
