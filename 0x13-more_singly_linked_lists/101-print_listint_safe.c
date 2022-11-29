fcc4197693426a2576fcbb17b3c8acc30c7f69da
sbonele <sbonelemiya@gmail.com>
2022-11-29 06:16:29 +0200

    low level

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,13 +1,7 @@
-/*
- * File: 101-print_listint_safe.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
 #include <stdio.h>
 
 size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
 
 /**
  * looped_listint_len - Counts the number of unique nodes
